6870f1628f543e0631ab28a063c0dce1c7fc7ec8
Phoebe <phoebe7979@Brandons-MacBook-Pro.local>
2019-09-08 02:49:26 -0400

    finished home page and about page

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+// When the user scrolls the page, execute myFunction//
+window.onscroll = function() {scrollbarFunction()};
+
+function scrollbarFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("bar").style.width = scrolled + "%";
+}
+
+
+
 /*Top navigation toggle menu*/
 function navIconClick() {
     var x = document.getElementById("nav-bar");
